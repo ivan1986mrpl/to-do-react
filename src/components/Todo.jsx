b237fd04e899e8c3811b05080fb708e673c23b3a
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-10-22 12:37:12 +0300

    feat: add props

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,13 +4,24 @@ import TodoInfo from "./TodoInfo"
 import TodoList from "./TodoList"
 
 const Todo = () => {
+  const tasks = [
+    {id: "task-1", title: "bay milk1", isDone: false},
+    {id: "task-2", title: "bay bread2", isDone: true},
+    {id: "task-3", title: "bay butter3", isDone: false},
+    {id: "task-4", title: "bay orange4", isDone: true},
+    {id: "task-5", title: "bay apple5", isDone: false},
+  ]
+
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
       <AddTaskForm />
       <SearchTaskForm />
-      <TodoInfo />
-      <TodoList />
+      <TodoInfo
+        total={tasks.length}
+        done={tasks.filter(({ isDone }) => isDone).length}
+      />
+      <TodoList tasks={tasks} />
     </div>
   )
 }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,40 +1,48 @@
-const TodoItem = () => {
+const TodoItem = (props) => {
+  const {
+    className = "",
+    id,
+    title,
+    isDone,
+  } = props
+
   return (
-        <li className="todo__item todo-item">
-          <input
-            className="todo-item__checkbox"
-            id="task-1"
-            type="checkbox"
-            checked
+    <li className={`todo-item ${className}`}>
+      <input
+        className="todo-item__checkbox"
+        id={id}
+        type="checkbox"
+        checked={isDone}
+        readOnly
+      />
+      <label
+        className="todo-item__label"
+        htmlFor={id}
+      >
+        {title}
+      </label>
+      <button
+        className="todo-item__delete-button"
+        aria-label="Delete"
+        title="Delete"
+      >
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M15 5L5 15M5 5L15 15"
+            stroke="#757575"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
-          <label
-            className="todo-item__label"
-            htmlFor="task-1"
-          >
-            Task 1
-          </label>
-          <button
-            className="todo-item__delete-button"
-            aria-label="Delete"
-            title="Delete"
-          >
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 5L5 15M5 5L15 15"
-                stroke="#757575"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
-        </li>
+        </svg>
+      </button>
+    </li>
   )
 }
 
