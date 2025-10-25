95bab71e24bda933d4834ae431479a5586f9e2b0
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-10-25 18:12:02 +0300

    init: start task linters

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** * @see https://prettier.io/docs/configuration * @type {import('prettier').Config} */
+export default {
+  // semi: false, //  Всегда не ставить точку с запятой
+  singleQuote: true, //  Использовать одинарные кавычки вместо двойных
+  tabWidth: 2,
+  endOfLine: 'lf',
+  bracketSpacing: true, //  Пробелы между скобками объектов
+  printWidth: 80,
+};
